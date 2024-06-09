514b20691bd2738bd5aa6b02b2fff0f1eab11a13
danimtb <danimanzaneque@gmail.com>
2024-06-09 18:54:26 +0200

    fix streams

--- config/configuration.py
+++ config/configuration.py
@@ -7,7 +7,7 @@ from model.node import Node
 class Configuration:
 
     def __init__(self, base_path):
-        self.nodes = None
+        self.nodes = []
         self.path = os.path.join(os.getcwd(), "configuration.yml")
         with open(self.path, 'r') as file:
             self.config = yaml.safe_load(file)
@@ -17,10 +17,9 @@ class Configuration:
 
     def get_nodes(self):
         if not self.nodes:
-            nodes = []
             for n in self.config["nodes"]:
                 streams = n["streams"] if "streams" in n.keys() else []
-                nodes.append(Node(n["id"], n["name"], streams))
+                self.nodes.append(Node(n["id"], n["name"], streams))
         return self.nodes
 
     def get_node_for_id(self, id):
